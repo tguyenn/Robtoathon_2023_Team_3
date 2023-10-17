7e2512fcc3be397950f55c105884a879cf59a0b7
tguyenn <tobyanguyen1@gmail.com>
2023-10-16 20:11:32 -0500

    forgor to save oops

--- sample_code/wall_detection.cpp
+++ sample_code/wall_detection.cpp
@@ -6,5 +6,14 @@
 
 #include <ESP32SharpIR.h>
 
+ESP32SharpIR left(ESP32SharpIR::GP2Y0A21YK0F, 36);
 
+void setup()  {
 // setiflterrate -> sensor gathers data in bursts... getdistancefloat gives average of bursts
+    left.setFilterRate(0.1f);
+}
+
+void loop() {
+    Serial.println(left.getDistanceFloat());
+    delay(500);
+}
