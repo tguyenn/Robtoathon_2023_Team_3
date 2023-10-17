f5bc997f6b3122981b8ece9f1df6082109c2828c
tguyenn <tobyanguyen1@gmail.com>
2023-10-16 22:17:21 -0500

    IR sensor base code

--- sample_code/wall_detection.cpp
+++ sample_code/wall_detection.cpp
@@ -1,19 +1,27 @@
+#include "sdkconfig.h"
+#ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
+#error "Must only be compiled when using Bluepad32 Arduino platform"
+#endif  // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 
+#include <Arduino.h>
+#include <Bluepad32.h>
 
-// ONLY PLUG IR SENSOR INTO 3.3V POWER SUPPLY
-// RED IS GROUND
-// BLACK IS POWER (lol)
-
+#include <ESP32Servo.h>
 #include <ESP32SharpIR.h>
+#include <QTRSensors.h>
 
 ESP32SharpIR left(ESP32SharpIR::GP2Y0A21YK0F, 36);
 
 void setup()  {
-// setiflterrate -> sensor gathers data in bursts... getdistancefloat gives average of bursts
-    left.setFilterRate(0.1f);
+    Serial.begin(115200);
+    // left.setFilterRate(0.1f); // works if you adjust filter rate properly (?)
 }
 
 void loop() {
-    Serial.println(left.getDistanceFloat());
+
+    Serial.println("test");
+    // Serial.println(left.getDistanceFloat()); // this function doesnt work for whatever reason idrk why. 
+    // it might be because the filter rate was not right??
+    Serial.println(left.getRawDistance()); // works and produces stable output but not in cm or units of measure
     delay(500);
 }
