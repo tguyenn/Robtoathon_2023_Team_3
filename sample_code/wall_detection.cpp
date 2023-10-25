7089e5b27d22617fb4593d633ac4e6a944a6311d
Drayy02 <clarkandray03@gmail.com>
2023-10-25 16:25:44 -0500

    Added working IR code in sample code

--- sample_code/wall_detection.cpp
+++ sample_code/wall_detection.cpp
@@ -10,21 +10,51 @@
 #include <ESP32SharpIR.h>
 #include <QTRSensors.h>
 
-// current setup only works from range of 9 cm to ~22 cm
-// 3 cm to 9 cm outputs 9.00 cm
-// 0 cm to 3 cm outputs bad values
-// CHANGE VALUES OF SET FILTERRATE TO GET BETTER RESULTS(?)
+Servo servo1; // declare servo objects
+Servo servo2;
 
-ESP32SharpIR left(ESP32SharpIR::GP2Y0A21YK0F, 36);
+ESP32SharpIR front(ESP32SharpIR::GP2Y0A21YK0F, 36);
+ESP32SharpIR right(ESP32SharpIR::GP2Y0A21YK0F, 39);
+ESP32SharpIR left(ESP32SharpIR::GP2Y0A21YK0F, 34);
 
 void setup()  {
     Serial.begin(115200);
-    left.setFilterRate(0.5f); // works if you adjust filter rate properly (?)
+    front.setFilterRate(0.5f);
+    left.setFilterRate(0.5f);
+    right.setFilterRate(0.5f);
+    servo1.attach(15);
+    servo2.attach(2);
 }
 
+// servo 1 ( left side ) <1500 foward, >1500 backwards
+// servo 2 ( right side ) >1500 foward, <1500 backwards
+
 void loop() {
-    Serial.println(left.getDistanceFloat()); // this function doesnt work for whatever reason idrk why. 
-    // it might be because the filter rate was not right??
-    // Serial.println(left.getRawDistance()); // works and produces stable output but not in cm or units of measure?
-    delay(100);
+    Serial.print("Front Sensor: ");
+    Serial.println(front.getDistanceFloat());
+    Serial.print("Left Sensor: "); 
+    Serial.println(left.getDistanceFloat());
+    Serial.print("Right Sensor: ");
+    Serial.println(right.getDistanceFloat());
+
+    if(front.getDistanceFloat() >= 10.00){
+        Serial.println("Continue Straight");
+        servo1.writeMicroseconds(1000);
+        servo2.writeMicroseconds(2000);
+    }
+    else if((front.getDistanceFloat() < 10.00)&&(right.getDistanceFloat() >= 10.00)&&(left.getDistanceFloat() < 10.00)) {
+        Serial.println("Turn Right");
+        servo1.writeMicroseconds(1000);
+        servo2.writeMicroseconds(1000);
+    }   
+    else if((front.getDistanceFloat() < 10.00) && (left.getDistanceFloat() >= 10.00) && (right.getDistanceFloat() >= 10.00)) {
+        Serial.println("Turn Left");
+        servo1.writeMicroseconds(2000);
+        servo2.writeMicroseconds(2000);
+    }
+    else{
+        Serial.println("Stop");
+        servo1.writeMicroseconds(1500);
+        servo2.writeMicroseconds(1500);
+    }
 } 
