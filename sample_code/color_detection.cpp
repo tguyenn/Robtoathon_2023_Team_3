8161de48e0ebda4c8b9071e166c57f4811b48c8b
tguyenn <122428794+tguyenn@users.noreply.github.com>
2023-10-28 13:10:20 -0500

    Update color_detection.cpp

--- sample_code/color_detection.cpp
+++ sample_code/color_detection.cpp
@@ -26,9 +26,10 @@ void setup() {
 
 
 
-char last_color_detected = 'r';
+char color_detector(int r, int g, int b, int a) {
 
-char return_strongest_color_detected(int r, int g, int b, int a) {
+    //this chunk of code is the code that has us identify whatever color we are CURRENTLY on.
+    //It is NOT for long term storage of the sample color. 
     int value=r;
     char char_to_return = 'r';
     if (g>value) {
@@ -43,42 +44,138 @@ char return_strongest_color_detected(int r, int g, int b, int a) {
     //currently doing nothing with clear
     return char_to_return;
 }
+void color_challenge() {
+    //This counter is just to keep track of time. 
+    int counter = 0;
+    //this boolean says if we have found the final color or not. 
+    //Once it's set to true, the while loop will end and so will the function. 
+    bool final_color_found = false;
 
+    //this bool says if we have found any new colors besides the original one 
+    bool new_color_found = false;
 
-// todo: 
-//      store greatest color int in int goalColor
-//      needs findButtonColor method
-//      if (r > g), store, etc.
+    //this guy holds whatever the sample color is
+    char sample_color == 'z';
 
-
-void loop() {
+    // these guys are helper variables for the color_detector function
     int r, g, b, a;
-    int upper, lower;
-    upper = 20;
-    lower = 10; //adjust based on testing
 
-    //wait until color is read from sensor
+    //this guy holds whatever color we are on rn. 
+    char last_color_detected = 'r';
+    
+    //this guy helps with the part of the code that flashes a certain number of times 
+    //to signal what color the sample was
+    bool flash_has_happened = false;
+
+    //This thing needs to run right at the beginning it's to initialize the color or something idk
     while (!apds.colorAvailable()) {
         delay(5);
     }
 
-    //read color from sensor
-    apds.readColor(r, g, b, a); //assigns color ints a value based on sensor
+    while (final_color_found == false) {
+        
+        //these two lines help with the function that determines what color we are currently on.
+        apds.readColor(r, g, b, a); //assigns color ints a value based on sensor
+        last_color_detected = color_detector(r,g,b,a); //takes in color ints and returns whatever the strongest one is
 
-    //print color in decimal
-    Serial.print("RED: ");
-    Serial.println(r);
-    Serial.print("GREEN: ");
-    Serial.println(g);
-    Serial.print("BLUE: ");
-    Serial.println(b);
-    Serial.print("AMBIENT(IGNORE): ");
-    Serial.println(a);
+        //This if statement will run a few times right at the beginning of our program
+        //it's purpose is to identify what the sample color that we need to store is.
+        //this statement should not be running at all after the first couple seconds
+        if ((counter>50) and (counter<90))
+        {
+            sample_color = last_color_detected;
+        }
 
-    last_color_detected = return_strongest_color_detected(r,g,b,a);
+        //rest of the time we need to be moving forward and scanning what color we are on
+        //THIS LOGIC ASSUMES THAT, ON THE COLOR STRIP, THE COLOR WE NEED TO STOP ON IS NOT
+        //IMMEDIATELY AFTER THE SAMPLE COLOR. IF IT IS WE ARE GONNA HAVE ISSUES ):
 
-    Serial.println("last color detected is ");
-    Serial.print(last_color_detected);
-    Serial.println(" ");
+        //if counter>100 that means it's go time baby. we gotta MOVE
+        if (counter>100 ) {
+
+            //this if statement runs ONE TIME. It flashes a few times to let the user know what sample color
+            //it thinks is correct.
+            if (flash_has_happened == false) {
+
+                if (sample_color == 'r') {
+                    //do red flash
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    Serial.println("SAMPLE COLOR FINAL IS RED")
+
+                }
+                else if (sample_color == 'g') {
+                    //do green flash
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    Serial.println("SAMPLE COLOR FINAL IS GREEN")
+                }
+                else if (sample_color == 'b') {
+                    //do blue flash
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    digitalWrite(LED,HIGH);
+                    delay(1000);
+                    digitalWrite(LED,LOW);
+                    delay(1000);
+                    Serial.println("SAMPLE COLOR FINAL IS BLUE");
+                }
+
+                flash_has_happened = true;
+            }
+
+
+            //move forward continuously
+            servo1.writeMicroseconds(1000);
+            servo2.writeMicroseconds(2000);
+
+
+            if ((last_color_detected == sample_color) && (new_color_found == true)) {
+
+                Serial.println("I HAVE FOUND A COLOR MATCHING THE SAMPLE COLOR");
+                final_color_found = true; 
+                //END PROGRAM
+
+            }
+            //This should trigger as soon as we run into a new color 
+            else if ((last_color_detected != sample_color) && (new_color_found == false)) {
+                new_color_found = true;
+                Serial.println("I am JUST NOW making it off of the sample color");
+            }
+            //This should be triggering if we are actively traversing and we are on a new color 
+            // but it's not the one we want to stop on
+            else if ((last_color_detected != sample_color) && (new_color_found== true)) {
+                Serial.println("I am off of the sample color.");
+            }
+            //this should be triggering if we are still on the sample color
+            else if ((last_color_detected == sample_color) && ( new_color_found == false)) {
+                Serial.println("I am still on the sample color!");
+            }
+
+        }
+        counter+=1; 
+    }
+
+
 
 }
